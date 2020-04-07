d3871b2324d897f171995a5a1eb453aab721f3a6
Kostia <kostyaetherell@gmail.com>
2020-04-07 03:41:58 +0300

    adding search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show]
+
 	def index
-		@articles = Article.all
+		@articles = Article.where(["title LIKE ?","%#{params[:search]}%"])
 	end
 
 	def show
